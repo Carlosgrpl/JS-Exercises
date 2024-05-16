2510e303201d6c4656262ef075ce33499eb2bf50
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-05-16 19:02:16 +0200

    Exercise 1, from S7_Arrays_Map

---
+++ S7/4.map/exercise-1.js
@@ -0,0 +1,18 @@
+/* Es un array de objetos, el más habitual */
+
+const users = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+
+const usersName = users.map((user) => {
+  return user.name;
+});
+
+console.log(usersName);
+
+/* Map nos devuelve  un nuevo array con la misma longitud que el original y con los mismos valores. Pero podemos decidir que informacióno queremos que devuelva */
+
+/* Map recibe un call back: () => { } */
