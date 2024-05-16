6d5bce8ad0b44fe6d8d6d0a78f3ae29ad63829b5
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-05-16 18:52:28 +0200

    Spread operator ex1 and ex2

---
+++ S7/3.spread-operators/exercise-1.js
@@ -0,0 +1,6 @@
+const pointsList = [32, 54, 21, 64, 75, 43];
+
+/* Los 3 puntos son el spread operator, lo que hace poner eso antes del nombre de la variable es desagregar el array */
+
+const pointsListCopy = [...pointsList];
+/* Así copiamos el array */
---
+++ S7/3.spread-operators/exercise-2.js
@@ -0,0 +1,7 @@
+const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+
+const toyCopy = { ...toy };
+
+console.log(toyCopy);
+
+/* No se suele usar el spread operator con objetos, porque no es una buena practica y es mejor usar el operador de propagación */
